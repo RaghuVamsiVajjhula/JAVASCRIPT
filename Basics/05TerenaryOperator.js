b83f7e239edac92fe75d4a477331bc421e517b11
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-06 17:04:02 +0530

    Add files via upload

---
+++ Basics/05TerenaryOperator.js
@@ -0,0 +1,10 @@
+// It is a method of using the if-else statement.
+// It takes the form of condition ? value1 : value2, 
+// where condition is a boolean expression, and 
+// value1 and value2 are expressions of any type.
+
+let x=20;
+let y=30;
+
+max=(x>y)? x:y;
+console.log(max);
