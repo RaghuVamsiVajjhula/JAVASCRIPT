8ccb1779a5c3079c0de8628d69cc3357971cd2d4
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-08 17:14:05 +0530

    Add files via upload

---
+++ Basics/09 Console-Error.js
@@ -0,0 +1,5 @@
+// We can print the error in the web browser by using this.
+console.error("This is an error.");
+// In an if or elif we can use this to get as an  output if there is an error.
+
+// check 09 console error.jpg for an example.
