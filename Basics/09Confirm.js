651464cc42240bf60e89cee0cf6a18a5dcf71e95
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-08 17:14:21 +0530

    Add files via upload

---
+++ Basics/09Confirm.js
@@ -0,0 +1,4 @@
+confirm("How are you?");
+// on running this on browser we get a popup asking
+// How are you? and there are 2 options Okay and cancel if you
+// choose okay it returns the value true and cancel returns value false.
