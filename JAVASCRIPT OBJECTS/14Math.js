9726e21d53e6bd7b971bd5dd59e0d70b52ee8265
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-09 10:49:05 +0530

    Add files via upload

---
+++ JAVASCRIPT OBJECTS/14Math.js
@@ -0,0 +1,24 @@
+// JavaScript Math object is a built-in object that provides a 
+//  variety of mathematical functions and constants.
+
+// Math.random() function, which generates a random number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.random());
+
+// Math.floor used to round a number down to its nearest integer.
+console.log(Math.floor(3.8));
+
+// Math.ceil() used to round a number up to its nearest integer.
+console.log(Math.ceil(3.2));
+
+// Math.max() and Math.min() used to get maximum and minimum values of a set.
+console.log("Maximum number:");
+console.log(Math.max(1,2,3,4,5));
+console.log("Minimum number:");
+console.log(Math.min(1,2,3,4,5));
+
+// Math object consists of most commonly used math constant like pi and e
+
+// Math.PI
+console.log(Math.PI);
+// Math.E
+console.log(Math.E);
