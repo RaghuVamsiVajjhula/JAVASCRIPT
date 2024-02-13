db2bd6b27fbe776c08f1d5b40860d1e1a9c0358c
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-13 16:12:29 +0530

    Add files via upload

---
+++ Exersices/04.js
@@ -0,0 +1,7 @@
+// program to change the URL to google.com(Redirection) if he enters number > 4
+
+var number=prompt("Enter a Number");
+if(number>4)
+{
+    location.href= "http://www.google.com";
+}
