237e9d1bb4687349d753422967f2ff01f33ccdc3
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-13 16:12:56 +0530

    Add files via upload

---
+++ Exersices/05.js
@@ -0,0 +1,5 @@
+// Change background of webpage by taking an input from user.
+// As it is example here, assume he is giving a valid color.
+
+var color=prompt("Enter any color in quotes:");
+document.body.style.background=color;
