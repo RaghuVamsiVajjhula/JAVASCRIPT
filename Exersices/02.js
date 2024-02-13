f680f6111831d15a23bea7b4490c596c8580b811
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-13 16:11:34 +0530

    Add files via upload

---
+++ Exersices/02.js
@@ -0,0 +1,20 @@
+// In the 01 question use confirm to ask if he needs the prompt to run again.
+function ask(){
+    var age=prompt("Enter your age:");
+if(age>=18)
+{
+    alert("You can drive");
+}
+else
+{
+    alert("You cannot drive");
+}
+}
+
+
+
+ask();
+var value=confirm("You want to see the prompt again?");
+if(true){
+    ask();    
+}
