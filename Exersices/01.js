6668fd1ef3e431904120838a14ee10531e2a8464
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-13 16:11:02 +0530

    Add files via upload

---
+++ Exersices/01.js
@@ -0,0 +1,14 @@
+// Write a progam using prompt function to take input of age as value from user
+// and use alert to tell him if he can drive!
+
+var age=prompt("Enter your age:");
+if(age>=18)
+{
+    alert("You can drive");
+}
+else
+{
+    alert("You cannot drive");
+}
+
+// This will work on browser  not on vs code.
