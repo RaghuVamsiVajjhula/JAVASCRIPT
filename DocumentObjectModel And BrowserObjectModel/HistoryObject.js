2844e8308593678d29cf484b34eb2b17c803ec0f
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-21 07:22:19 +0530

    Add files via upload

---
+++ DocumentObjectModel And BrowserObjectModel/HistoryObject.js
@@ -0,0 +1,11 @@
+// History object is a part of window object that provides access to browsers history.
+// It allows developers to manipulate the browsers history and
+// Change the current URL without reloading the page.
+
+// Most commonly used methods in history is: pushState() method.
+
+//This method allows developers to add new entry in browsers history and
+// change current url without any reloading of page.
+history.pushState(null, null, "https://www.example.com");
+// above is the syntax.
+
