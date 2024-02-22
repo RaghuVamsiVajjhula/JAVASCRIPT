c90afe481f977f4e4e10ad5b68bd59df6b0465b7
VamsiVajjhula <117925570+RaghuVamsiVajjhula@users.noreply.github.com>
2024-02-22 06:45:15 +0530

    Add files via upload

---
+++ DocumentObjectModel And BrowserObjectModel/Element only navigation continuation/script.js
@@ -0,0 +1,7 @@
+const changebg=()=>{
+    document.body.firstElementChild.style.background="red"
+  }
+  
+  // use changebg() in the console of a browser to make the background of the firstelementchild red.
+  
+  
